372190b1246dd0c6980f9edb882c27bfbfc4431d
Peltogle <42919137+peltogle@users.noreply.github.com>
2023-03-19 00:09:00 -0700

    basic main page concept

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Blank readme
+# CMPE 131 Team 2 Group Project
+
+Blank readme, someone update this :)
+
+## Reminders:
+- We need to figure out the automated code testing part which the professor wants
--- flask_folder/app.py
+++ flask_folder/app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello, Flask!'
+    return send_from_directory('static', 'index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
