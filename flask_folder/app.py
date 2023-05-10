59455654881c0fe168bde9f3a7e4f4a271ca0259
Peltogle <42919137+peltogle@users.noreply.github.com>
2023-05-10 14:39:03 -0700

    Support for removing items, and shipping cost.

--- flask_folder/app.py
+++ flask_folder/app.py
@@ -6,8 +6,6 @@ supabase: Client = create_client("https://yxvtigsplpdppgwlktpn.supabase.co",
                                  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4dnRpZ3NwbHBkcHBnd2xrdHBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzkyMDUwNzMsImV4cCI6MTk5NDc4MTA3M30.S_V8wk3u2hKG5p2XL5TlQfYGwYxzNh488Y7vzz-UTXY")
 
 
-# TODO make global cart class to make things more smooth
-
 @app.context_processor
 def inject_globals():
     return {'green_salad_url': 'https://yxvtigsplpdppgwlktpn.supabase.co/storage/v1/object/public/website_assets/website_images/green_salad.png'}
@@ -185,7 +183,6 @@ def signOut():
     return redirect(url_for('index'))
 
 
-# TODO: fix being able to go to these links when not signed in
 @app.route('/orders')
 def orders():
     if returnActiveSession():
@@ -214,10 +211,14 @@ def cart():
                 "price"), catalogInfo.get("weight"), userCart.items.get(key), catalogInfo.get("item_id")))
         if request.method == 'POST':
             itemId = request.form['itemId']
-        if not itemId:
-            flash('Id is required!')
-        else:
-            userCart.removeItem(itemId)
+            if not itemId:
+                flash('Id is required!')
+            else:
+                for item in userItems:
+                    if item.itemId == itemId:
+                        userItems.remove(item)
+                userCart.removeItem(itemId)
+                return redirect(url_for('cart'))
         return render_template('cart.html',
                                activePage="cart",
                                pageTitle="Cart",
@@ -228,6 +229,7 @@ def cart():
                                totalCost=userCart.totalCost,
                                shippingCost=userCart.shippingCost,
                                userItems=userItems)
+    # TODO: remove subtotal
     else:
         return redirect(url_for('index'))
 
@@ -264,7 +266,7 @@ class Cart:
     """ 
     # start Class constructor
     #
-    Check if cart entry exists for user, if not, create one
+    [ ] TODO: Check if cart entry exists for user, if not, create one
     [-] If a cart entry exists run countItems, calcShipping, and calcTotal
     #
     # end Class constructor
@@ -276,7 +278,7 @@ class Cart:
         self.supaResponse = supabase.table('carts').select(
             '*').eq('created_by', self.uuid).limit(1).execute()
         # Check if a cart entry exists for the user already
-        if False:  # TODO: self.supaResponse.count is None
+        if False:  # self.supaResponse.count is None
             supabase.table('carts').insert(
                 {'created_by': self.uuid, 'items': {}}).execute()
         self.items = dict(supabase.table('carts').select('items').eq(
@@ -287,8 +289,8 @@ class Cart:
     # start calcData
     #
     [-] Calc total items
-    [ ] Check weights of all items in cart, it it exceeds 20lb add $5 shipping
-    [ ] Calculate total cost of all items in cart plus shipping
+    [-] Check weights of all items in cart, it it exceeds 20lb add $5 shipping
+    [-] Calculate total cost of all items in cart plus shipping
     #
     # end calcData
     """
@@ -296,12 +298,25 @@ class Cart:
     def calcData(self):
         # Calc total items
         self.itemCount = len(self.items)
+        # Check item weights
+        totalWeight = 0
+        for key in self.items:
+            catalogInfo = dict(supabase.table('catalog').select('*').eq('item_id', key).limit(1).execute())["data"][0]
+            totalWeight += (catalogInfo.get("weight") * int(self.items.get(key)))
+        if totalWeight >= 20:
+            self.shippingCost = True
+            self.totalCost += 5
+        # Calc total cost plus shipping
+        for key in self.items:
+            catalogInfo = dict(supabase.table('catalog').select('*').eq('item_id', key).limit(1).execute())["data"][0]
+            self.totalCost += (int(self.items.get(key)) * catalogInfo.get("price"))
+
 
     """ 
     # start addItem
     #
     [-] Check if item already is in cart, if so add the additional quantity to the cart, if not add to cart normally
-    [ ] If the quantity in the cart exceeds stock, trim down to the highest amount that can be ordered and issue error
+    [ ] TODO: If the quantity in the cart exceeds stock, trim down to the highest amount that can be ordered and issue error
     [-] Recalculate data
     #
     # end addItem
@@ -319,19 +334,25 @@ class Cart:
     """ 
     # start removeItem
     #
-    [ ] Remove the item (check if item in list)
-    [ ] Recalculate data
+    [-] Remove the item (check if item in list)
+    [-] Recalculate data
     #
     # end removeItem
     """
     def removeItem(self, itemId):
-        pass
+        if itemId not in self.items:
+            flash('item not in list!')
+        else:
+            self.items.pop(itemId)
+        supabase.table('carts').update({"items": self.items}).eq(
+            'created_by', self.uuid).execute()  
+        self.calcData()
 
     """ 
     # start placeOrder
     #
-    [ ] Update stock then place order, send order to orders catalog so that user can create a new order, clear cart catalog entry
-    [ ] Redirect to orders page
+    [ ] TODO: Update stock then place order, send order to orders catalog so that user can create a new order, clear cart catalog entry
+    [ ] TODO: Redirect to orders page
     #
     # end placeOrder
     """
