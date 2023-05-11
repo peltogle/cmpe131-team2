50b94dad6c179c511ad05858abcd05ddc321ae2e
Peltogle <42919137+peltogle@users.noreply.github.com>
2023-05-11 16:27:52 -0700

    Final update

--- flask_folder/app.py
+++ flask_folder/app.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, flash, redirect, url_for, request, ses
 SUPABASE_URL = "https://yxvtigsplpdppgwlktpn.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4dnRpZ3NwbHBkcHBnd2xrdHBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzkyMDUwNzMsImV4cCI6MTk5NDc4MTA3M30.S_V8wk3u2hKG5p2XL5TlQfYGwYxzNh488Y7vzz-UTXY"
 SALAD_URL = "https://yxvtigsplpdppgwlktpn.supabase.co/storage/v1/object/public/website_assets/website_images/green_salad.png"
-VERSION = "0.3.3"
+VERSION = "0.3.4"
 
 # Set locale for float to dollar conversion
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
@@ -245,15 +245,20 @@ def auth(type):
             elif not userPassword:
                 flash('Password is required!')
             elif type == 'signup':
-                supabase.auth.sign_up(
-                    {"email": userEmail, "password": userPassword})
+                try:
+                    supabase.auth.sign_up({"email": userEmail, "password": userPassword})
+                except Exception as e:
+                    print(str(e))
+                    return redirect(url_for('errors', error=str(e)))
                 session['uid'] = supabase.auth.get_session().user.id
                 fetch_state()
                 supabase.auth.sign_out()
                 return redirect(url_for('index'))
             elif type == 'signin':
-                supabase.auth.sign_in_with_password(
-                    {"email": userEmail, "password": userPassword})
+                try:
+                    supabase.auth.sign_in_with_password({"email": userEmail, "password": userPassword})
+                except Exception as e:
+                    return redirect(url_for('errors', error=str(e)))
                 session['uid'] = supabase.auth.get_session().user.id
                 fetch_state()
                 supabase.auth.sign_out()
@@ -267,6 +272,12 @@ def auth(type):
                                activeSession=session['logged_in'])
     else:
         return redirect(url_for('index'))
+
+@app.route('/errors')
+def errors():
+    error_message = request.args.get('error')
+    return render_template('error.html', error_message=error_message)
+
 
 # A class to help display items in the cart on the cart page
 
